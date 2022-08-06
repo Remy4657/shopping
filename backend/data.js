e036793183dc58b03146e2fe3e5fd40ee20a104c
Remy4657 <trongdatga@gmail.com>
2022-08-06 15:37:07 +0700

    add bootstrap

--- backend/data.js
+++ backend/data.js
@@ -1,9 +1,10 @@
 const data = {
   products: [
     {
-      id: 1,
+      id: "1",
       price: 100,
       img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+      countInstock: 100,
       title: "SUMMER SALE",
       desc: "Loại áo này “khó tính” hơn áo thun nam nữ ngắn tay, nếu biết cách mix đồ, bạn sẽ trở nên thật cá tính với phong cách thời trang đậm chất Hàn Quốc, nhưng nếu phối đồ không tốt trông bạn như đang “lọt thỏm” trong chiếc áo thun tay lỡ.",
     },
@@ -11,6 +12,7 @@ const data = {
       id: 2,
       price: 50,
       img: "https://salt.tikicdn.com/cache/400x400/ts/product/70/11/8c/e4ef5e543dd0ef5ad42f203a03eeefdf.jpg.webp",
+      countInstock: 100,
       title: "AUTUMN COLLECTION",
       desc: "Loại áo này “khó tính” hơn áo thun nam nữ ngắn tay, nếu biết cách mix đồ, bạn sẽ trở nên thật cá tính với phong cách thời trang đậm chất Hàn Quốc, nhưng nếu phối đồ không tốt trông bạn như đang “lọt thỏm” trong chiếc áo thun tay lỡ.",
     },
@@ -18,6 +20,7 @@ const data = {
       id: 3,
       price: 200,
       img: "https://salt.tikicdn.com/cache/400x400/ts/product/e5/3e/08/58819ea7d03d498089bb8f0d79ad41fb.jpg.webp",
+      countInstock: 100,
       title: "LOUNGEWEAR LOVE",
       desc: "Loại áo này “khó tính” hơn áo thun nam nữ ngắn tay, nếu biết cách mix đồ, bạn sẽ trở nên thật cá tính với phong cách thời trang đậm chất Hàn Quốc, nhưng nếu phối đồ không tốt trông bạn như đang “lọt thỏm” trong chiếc áo thun tay lỡ.",
     },
@@ -26,6 +29,7 @@ const data = {
       desc: "Loại áo này “khó tính” hơn áo thun nam nữ ngắn tay, nếu biết cách mix đồ, bạn sẽ trở nên thật cá tính với phong cách thời trang đậm chất Hàn Quốc, nhưng nếu phối đồ không tốt trông bạn như đang “lọt thỏm” trong chiếc áo thun tay lỡ.",
       price: 100,
       img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      countInstock: 100,
       title: "SHIRT STYLE!",
     },
     {
@@ -33,6 +37,7 @@ const data = {
       desc: "Loại áo này “khó tính” hơn áo thun nam nữ ngắn tay, nếu biết cách mix đồ, bạn sẽ trở nên thật cá tính với phong cách thời trang đậm chất Hàn Quốc, nhưng nếu phối đồ không tốt trông bạn như đang “lọt thỏm” trong chiếc áo thun tay lỡ.",
       price: 300,
       img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      countInstock: 100,
       title: "LOUNGEWEAR LOVE",
     },
     {
@@ -40,6 +45,7 @@ const data = {
       desc: "Loại áo này “khó tính” hơn áo thun nam nữ ngắn tay, nếu biết cách mix đồ, bạn sẽ trở nên thật cá tính với phong cách thời trang đậm chất Hàn Quốc, nhưng nếu phối đồ không tốt trông bạn như đang “lọt thỏm” trong chiếc áo thun tay lỡ.",
       price: 500,
       img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      countInstock: 100,
       title: "LIGHT JACKETS",
     },
     {
@@ -47,6 +53,7 @@ const data = {
       desc: "Loại áo này “khó tính” hơn áo thun nam nữ ngắn tay, nếu biết cách mix đồ, bạn sẽ trở nên thật cá tính với phong cách thời trang đậm chất Hàn Quốc, nhưng nếu phối đồ không tốt trông bạn như đang “lọt thỏm” trong chiếc áo thun tay lỡ.",
       price: 200,
       img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+      countInstock: 100,
       title: "LOUNGEWEAR LOVE",
     },
     {
@@ -54,6 +61,7 @@ const data = {
       desc: "Loại áo này “khó tính” hơn áo thun nam nữ ngắn tay, nếu biết cách mix đồ, bạn sẽ trở nên thật cá tính với phong cách thời trang đậm chất Hàn Quốc, nhưng nếu phối đồ không tốt trông bạn như đang “lọt thỏm” trong chiếc áo thun tay lỡ.",
       price: 150,
       img: "https://mcdn2-coolmate.cdn.vccloud.vn/uploads/December2021/pros123.jpeg",
+      countInstock: 100,
       title: "LOUNGEWEAR LOVE",
     },
     {
@@ -62,6 +70,7 @@ const data = {
       title: "LOUNGEWEAR LOVE",
       price: 90,
       img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+      countInstock: 100,
     },
     {
       id: 10,
@@ -69,6 +78,7 @@ const data = {
       title: "LOUNGEWEAR LOVE",
       price: 120,
       img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+      countInstock: 100,
     },
     {
       id: 11,
@@ -76,11 +86,13 @@ const data = {
       desc: "Loại áo này “khó tính” hơn áo thun nam nữ ngắn tay, nếu biết cách mix đồ, bạn sẽ trở nên thật cá tính với phong cách thời trang đậm chất Hàn Quốc, nhưng nếu phối đồ không tốt trông bạn như đang “lọt thỏm” trong chiếc áo thun tay lỡ.",
       price: 80,
       img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+      countInstock: 100,
     },
     {
       id: 12,
       price: 108,
       img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+      countInstock: 100,
       title: "LOUNGEWEAR LOVE",
       desc: "Loại áo này “khó tính” hơn áo thun nam nữ ngắn tay, nếu biết cách mix đồ, bạn sẽ trở nên thật cá tính với phong cách thời trang đậm chất Hàn Quốc, nhưng nếu phối đồ không tốt trông bạn như đang “lọt thỏm” trong chiếc áo thun tay lỡ.",
     },
@@ -88,6 +100,7 @@ const data = {
       id: 13,
       price: 108,
       img: "https://salt.tikicdn.com/cache/525x525/ts/product/f6/1e/3a/8b328a72d2a9986caa636dc6066c5310.jpg",
+      countInstock: 100,
       title: "LOUNGEWEAR LOVE",
       desc: "Loại áo này “khó tính” hơn áo thun nam nữ ngắn tay, nếu biết cách mix đồ, bạn sẽ trở nên thật cá tính với phong cách thời trang đậm chất Hàn Quốc, nhưng nếu phối đồ không tốt trông bạn như đang “lọt thỏm” trong chiếc áo thun tay lỡ.",
     },
@@ -95,6 +108,7 @@ const data = {
       id: 14,
       price: 108,
       img: "https://naidecor.vn/wp-content/uploads/2020/01/ct00192-549k-800x800.jpg",
+      countInstock: 100,
       title: "LOUNGEWEAR LOVE",
       desc: "Loại áo này “khó tính” hơn áo thun nam nữ ngắn tay, nếu biết cách mix đồ, bạn sẽ trở nên thật cá tính với phong cách thời trang đậm chất Hàn Quốc, nhưng nếu phối đồ không tốt trông bạn như đang “lọt thỏm” trong chiếc áo thun tay lỡ.",
     },
@@ -102,6 +116,7 @@ const data = {
       id: 15,
       price: 108,
       img: "https://hidanz.com/wp-content/uploads/2020/10/ao-thun-nam-co-tron2.jpg",
+      countInstock: 100,
       title: "LOUNGEWEAR LOVE",
       desc: "Loại áo này “khó tính” hơn áo thun nam nữ ngắn tay, nếu biết cách mix đồ, bạn sẽ trở nên thật cá tính với phong cách thời trang đậm chất Hàn Quốc, nhưng nếu phối đồ không tốt trông bạn như đang “lọt thỏm” trong chiếc áo thun tay lỡ.",
     },
@@ -109,6 +124,7 @@ const data = {
       id: 16,
       price: 180,
       img: "https://cf.shopee.vn/file/01fe62e8004ba1472183bbc72398124e",
+      countInstock: 100,
       title: "LOUNGEWEAR LOVE",
       desc: "Loại áo này “khó tính” hơn áo thun nam nữ ngắn tay, nếu biết cách mix đồ, bạn sẽ trở nên thật cá tính với phong cách thời trang đậm chất Hàn Quốc, nhưng nếu phối đồ không tốt trông bạn như đang “lọt thỏm” trong chiếc áo thun tay lỡ.",
     },
--- backend/server.js
+++ backend/server.js
@@ -3,25 +3,31 @@ import data from "./data.js";
 
 const app = express();
 
-app.all("/", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "Content-Type",
+    "Authorization"
+  );
   next();
 });
+
 //test
+app.get("/product/:id", (req, res) => {
+  const product = data.products.find((x) => x.id == req.params.id);
+
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
+});
 app.get("/", (req, res, next) => {
   res.send(data.products);
 });
-// app.get("/api/products/slug/:slug", (req, res) => {
-//   const product = data.products.find((x) => x.slug === req.params.slug);
-//   //console.log("product: ", product);
-//   //res.send(data.products);
-//   if (product) {
-//     res.send(product);
-//   } else {
-//     res.status(404).send({ message: "Product not found" });
-//   }
-// });
+
 const port = 8080;
 app.listen(port, () => {
   console.log(`server at http://localhost:${port}`);
