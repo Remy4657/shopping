a440645778617d61bfaf513dcacc4d4ccd645a0a
Remy4657 <trongdatga@gmail.com>
2022-12-21 23:55:11 +0700

    push backend again

--- backend/server.js
+++ backend/server.js
@@ -62,7 +62,7 @@ app.use(express.static(path.join(__dirname, "/frontend/build")));
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
 );
-//hghg
+//hghg ftft
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
